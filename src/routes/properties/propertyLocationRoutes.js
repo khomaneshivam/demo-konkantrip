22e645d8be55c5e54b704ddf3de80baf182d78ca
khomaneshivam <khomaneshivam@gmail.com>
2026-08-18 17:17:54 +0530

    mm

--- src/routes/properties/propertyLocationRoutes.js
+++ src/routes/properties/propertyLocationRoutes.js
@@ -1,17 +1,17 @@
 const express = require("express");
-const authMiddleware = require("../../middlewares/authMiddleware");
+const authMiddleware = require("../../middleware/authMiddleware");
 const {
     createPropertyLocation,
     deletePropertyLocation,
     getPropertyLocation,
     updatePropertyLocation
-} = require("../../controllers/properties/propertyLocations");
+} = require("../../controller/properties/propertyLocations");
 
 const router = express.Router();
 
-router.get("/location/:propertyId", getPropertyLocation);
-router.post("/location/:propertyId", authMiddleware, createPropertyLocation);
-router.put("/location/:propertyId", authMiddleware, updatePropertyLocation);
-router.delete("/location/:propertyId", authMiddleware, deletePropertyLocation);
+router.get("/:propertyId/location", getPropertyLocation);
+router.post("/:propertyId/location", authMiddleware, createPropertyLocation);
+router.put("/:propertyId/location", authMiddleware, updatePropertyLocation);
+router.delete("/:propertyId/location", authMiddleware, deletePropertyLocation);
 
 module.exports = router;
